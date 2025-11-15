7d7f2853af3fbd2e521da536c2b827f199b174ba
elysonia <elysoniaalim@gmail.com>
2025-11-15 20:47:49 +1000

    Use ripples from other tiles to activate a disabled tile

--- scenes/ripple/ripple.gd
+++ scenes/ripple/ripple.gd
@@ -6,6 +6,7 @@ const DEFAULT_WIDTH = 3
 const DEFAULT_RADIUS = 32
 const MAX_STRENGTH = 5
 const DEFAULT_STRENGTH = 3
+const STARTING_RIPPLE = 1.0
 
 var _collision_shape: CollisionShape2D
 
@@ -13,16 +14,20 @@ var _collision_shape: CollisionShape2D
 @export var width: int = DEFAULT_WIDTH
 @export var radius: int = DEFAULT_RADIUS
 @export var ripple_strength: int = DEFAULT_STRENGTH
-@export var current_ripple: float = 1.0
+@export var current_ripple: float = STARTING_RIPPLE
+
 @export var is_rippling: bool = false
 
 
-func handle_ripple(collision_shape: CollisionShape2D, strength: int = DEFAULT_STRENGTH) -> void:
+func initialize(collision_shape: CollisionShape2D) -> void:
+	_collision_shape = collision_shape
+
+
+func handle_ripple(strength: int = DEFAULT_STRENGTH) -> void:
 	ripple_strength = strength
 	current_ripple = 1.0
 	is_rippling = true
 	visible = true
-	_collision_shape = collision_shape
 
 
 func _draw():
@@ -31,16 +36,21 @@ func _draw():
 		var ripple_width = width * (ripple_strength / current_ripple)
 		var alpha = 1 - (current_ripple / ripple_strength)
 		draw_circle(position, ripple_radius, Color(1, 1, 1, alpha), false, ripple_width)
-		_collision_shape.shape.radius = ripple_radius
+
+		if _collision_shape:
+			_collision_shape.shape.radius = ripple_radius
 
 
 func _process(delta: float):
 	if current_ripple >= ripple_strength:
 		is_rippling = false
 		visible = false
-		_collision_shape.shape.radius = radius
-	else:
-		current_ripple += speed * delta
+		current_ripple = STARTING_RIPPLE
+		ripple_strength = DEFAULT_STRENGTH
+
+		if _collision_shape:
+			_collision_shape.shape.radius = radius
 
 	if is_rippling:
+		current_ripple += speed * delta
 		queue_redraw()
--- scenes/tile/tile.gd
+++ scenes/tile/tile.gd
@@ -1,17 +1,53 @@
 class_name Tile
 extends Area2D
 
+enum Status {ENABLED, DISABLED}
+const COLOR = {
+    Status.ENABLED: "#b9f84f",
+    Status.DISABLED: "#111c02",
+}
+
+var _is_mouse_entered: bool = false
+
+@export var status: Status = Status.ENABLED
+
+@export var is_status_fixed: bool = true
+
 
 func _ready():
     area_entered.connect(_on_area_entered)
     body_entered.connect(_on_body_entered)
-    %Ripple.handle_ripple(%CollisionShape2D)
+    mouse_entered.connect(_on_mouse_entered)
+    mouse_exited.connect(_on_mouse_exited)
+    %Polygon2D.color = COLOR[status]
+
+    # Make sure the collision shape of the instantiated scene is unique
+    # https://forum.godotengine.org/t/collisionshape2d-changes-for-every-instance-with-every-instance/95614/2
+    %CollisionShape2D.shape =  %CollisionShape2D.shape.duplicate(true)
+    %Ripple.initialize(%CollisionShape2D)
+
+
+func _unhandled_input(_event: InputEvent) -> void:
+    if Input.is_action_just_pressed("lmb") and _is_mouse_entered:
+        if status == Status.ENABLED and is_status_fixed:
+            %Ripple.handle_ripple()
 
 
 func _on_area_entered(area: Area2D) -> void:
-    pass
+    if not is_instance_of(area, Tile) or status == Status.ENABLED:
+        return
+
+    status = Status.ENABLED
+    %Polygon2D.color = COLOR[status]
 
 
 func _on_body_entered(body: Node2D) -> void:
-    %Ripple.handle_ripple(%CollisionShape2D)
+    pass
 
+
+func _on_mouse_entered() -> void:
+    _is_mouse_entered = true
+
+
+func _on_mouse_exited() -> void:
+    _is_mouse_entered = false
