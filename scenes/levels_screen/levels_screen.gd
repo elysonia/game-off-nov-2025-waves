726aa16251e3410f1a6d3fb7bf0854acc2196f03
elysonia <elysoniaalim@gmail.com>
2025-12-01 18:01:40 +1000

    Make sure players can view level details before playing

--- scenes/levels_screen/levels_screen.gd
+++ scenes/levels_screen/levels_screen.gd
@@ -9,6 +9,8 @@ var _level_map_instance = null
 func _ready():
 	%Play.disabled = true
 	%Play.pressed.connect(_on_play_pressed)
+	%Back.pressed.connect(_on_back_pressed)
+	%PreviewDetailContainer.visible = false
 
 
 func initialize() -> void:
@@ -28,18 +30,28 @@ func _on_level_pressed(level: int) -> void:
 	if _previewed_level == level:
 		_previewed_level = 0
 		%Play.disabled = true
+		%PreviewDetailContainer.visible = false
 
 		if is_instance_valid(_level_map_instance):
 			_level_map_instance.queue_free()
 		return
 
 	_previewed_level = level
+	%PreviewDetailContainer.visible = true
+
+	for child in %PreviewDetails.get_children():
+		child.queue_free()
+
 	var level_map_instance = LevelManager.get_level_map(level).instantiate()
 
-	level_map_instance.preview()
 	_level_map_instance = level_map_instance
 	%Play.disabled = false
 	%Preview.add_child(level_map_instance)
+	level_map_instance.preview()
+	var preview_details = level_map_instance.get_preview_details(_previewed_level)
+
+	for detail in preview_details:
+		%PreviewDetails.add_child(detail)
 
 
 func _on_play_pressed() -> void:
@@ -47,3 +59,7 @@ func _on_play_pressed() -> void:
 		return
 
 	Utils.goto_level(_previewed_level)
+
+
+func _on_back_pressed() -> void:
+	queue_free()
